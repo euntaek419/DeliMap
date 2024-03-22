a901113cc20fd046460b38168eeaa14dc73d4879
cet4713@gmail.com <cet4713@gmail.com>
2024-03-22 17:56:21 +0900

    feat : 키워드로 장소검색하고 목록으로 표출하기

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     head: {
       script:[
         {
-          src:"//dapi.kakao.com/v2/maps/sdk.js?autoreload=false&appkey=2ede99203c8df1e5be305398646c566e"
+          src:"//dapi.kakao.com/v2/maps/sdk.js?autoreload=false&appkey=2ede99203c8df1e5be305398646c566e&libraries=services"
         }
       ]
     },
