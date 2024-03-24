091174d7157aa97b2dd484c8e9a1265da09d5213
cet4713@gmail.com <cet4713@gmail.com>
2024-03-24 14:53:31 +0900

    feat: 내 위치 확인, 마커, 인포윈도우 출력

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,9 @@ import Menu from './main/menu.vue'
 
 let keyword = ref('서울역')
 
+// 단일 마커
+let marker = null;
+
 // 마커를 담을 배열입니다
 let markers = [];
 
@@ -11,13 +14,40 @@ let map = null;
 let ps = null;
 let infowindow = null;
 
+let latlng = null;
+let message = null;
+
+let markerInfowindow = null;
 
 onMounted(() => {
   makeMap();
+  makeMarker();
+  geolocation();
+
+  // 지도에 클릭 이벤트를 등록합니다
+  // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+  kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+
+    if (markerInfowindow !== null) {
+      closeIW()
+    }
+
+    // 클릭한 위도, 경도 정보를 가져옵니다 
+    latlng = mouseEvent.latLng;
+
+    // 마커 위치를 클릭한 위치로 옮깁니다
+    marker.setPosition(latlng);
+
+    message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+    message += '경도는 ' + latlng.getLng() + ' 입니다';
+
+    makeMarkerInfowindow()
+
+  });
 })
 
 function makeMap() {
-  let mapContainer =document.getElementById('map'), // 지도를 표시할 div 
+  let mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
       center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
       level: 3 // 지도의 확대 레벨
@@ -36,6 +66,12 @@ function makeMap() {
   searchPlaces();
 }
 
+function makeMarker() {
+  marker = new kakao.maps.Marker();
+  // 지도에 마커를 표시합니다
+  marker.setMap(map);
+}
+
 // 키워드 검색을 요청하는 함수입니다
 function searchPlaces() {
 
@@ -46,6 +82,34 @@ function searchPlaces() {
 
   // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
   ps.keywordSearch(keyword.value, placesSearchCB);
+}
+
+function geolocation() {
+  // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
+  if (navigator.geolocation) {
+
+    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    navigator.geolocation.getCurrentPosition(function (position) {
+
+      var lat = position.coords.latitude, // 위도
+        lon = position.coords.longitude; // 경도
+
+      var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+        message = '<div style="padding:5px;">내 위치</div>'; // 인포윈도우에 표시될 내용입니다
+
+        console.log(message)
+      // 마커와 인포윈도우를 표시합니다
+      displayMarker(locPosition, message);
+
+    });
+
+  } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+    var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
+      message = 'geolocation을 사용할수 없어요..'
+
+    displayMarker(locPosition, message);
+  }
 }
 
 // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
@@ -229,6 +293,47 @@ function removeAllChildNods(el) {
     el.removeChild(el.lastChild);
   }
 }
+
+function makeMarkerInfowindow() {
+
+  // 인포윈도우를 생성합니다
+  markerInfowindow = new kakao.maps.InfoWindow({
+    // position: iwPosition,
+    content: message,
+  });
+
+  markerInfowindow.open(map, marker);
+}
+
+function closeIW() {
+  markerInfowindow.close()
+  markerInfowindow = null;
+}
+
+// 지도에 마커와 인포윈도우를 표시하는 함수입니다
+function displayMarker(locPosition, message) {
+
+  // 마커를 생성합니다
+  var marker = new kakao.maps.Marker({
+    map: map,
+    position: locPosition
+  });
+
+  var iwContent = message, // 인포윈도우에 표시할 내용
+    iwRemoveable = true;
+
+  // 인포윈도우를 생성합니다
+  var infowindow = new kakao.maps.InfoWindow({
+    content: iwContent,
+    removable: iwRemoveable
+  });
+
+  // 인포윈도우를 마커위에 표시합니다 
+  infowindow.open(map, marker);
+
+  // 지도 중심좌표를 접속위치로 변경합니다
+  map.setCenter(locPosition);
+}    
 </script>
 
 <template>
